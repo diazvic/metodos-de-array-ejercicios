e0e9b6ca0ddecd2b427664b0e6ccd38a1ab4d72d
Victoria Diaz <diazmariavictoria94@gmail.com>
2021-10-18 10:56:13 -0300

    ejercicio 7 finalizado

--- index.js
+++ index.js
@@ -92,3 +92,26 @@
 // console.log(musica);
 // RESULTADO ESPERADO
 // [ '0 - Everlong', '1 - The Pretender', '2 - Learn to Fly' ]
+
+//7 Tenemos un array en una variable costos con números que representan costos de diferentes productos.
+// Completar el siguiente código para llegar al resultado esperado
+// A un costo primero se le agrega la ganancia y después el IVA (este ultimo sobre el costo + la ganancia)
+
+const costos = [12.5, 56, 98, 45.75];
+
+const agregarIVA = (costo) => {
+	return costo * 1.21;
+};
+
+const sumarGanancia = (costo) => {
+	return costo * 1.5;
+};
+
+// codear acá la solución del ejercicio
+const preciosFinales = costos.map((elemento) => {
+	return agregarIVA(sumarGanancia(elemento));
+});
+
+console.log(preciosFinales);
+// deberia mostrar
+// [ 22.6875, 101.64, 177.87, 83.03625 ]
